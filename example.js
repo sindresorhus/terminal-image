c1a8e23384c7b4737a00f3cc2dde7463a38f9043
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2018-05-13 01:04:03 +0200

    Updated example

--- example.js
+++ example.js
@@ -1,6 +1,8 @@
+#!/usr/bin/env node
+
 'use strict';
 const terminalImage = require('.');
 
 (async () => {
-	console.log(await terminalImage.file('fixture.jpg'));
+	console.log(await terminalImage('fixture.jpg'));
 })();
