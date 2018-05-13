97c33f708606b08fcdc3fa2187df55250bed3ce7
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2018-05-13 22:26:46 +0200

    Don't generate repeated escape codes

--- index.js
+++ index.js
@@ -8,6 +8,50 @@ const terminalCharWidth = require('terminal-char-width');
 const CHAR_HEIGHT = 16;
 const CHAR_WIDTH = CHAR_HEIGHT * terminalCharWidth;
 
+/**
+ * Check if two colors are equal
+ *
+ * Both has `alpha=0`, or `alpha>0` and equl all the other color values
+ *
+ * @returns {Boolean} colors are equal
+ */
+function equalColor({r, g, b, a}, {r: r2, g: g2, b: b2, a: a2}) {
+	if (a === 0 && a2 === 0) {
+		return true;
+	}
+
+	return a > 0 && a2 > 0 && r === r2 && g === g2 && b === b2;
+}
+
+function getBgColor({r, g, b, a}) {
+	// Full transparent
+	if (a === 0) {
+		return chalk.reset;
+	}
+
+	return chalk.bgRgb(r, g, b);
+}
+
+function getFgColor({r, g, b}) {
+	return chalk.rgb(r, g, b);
+}
+
+function getGlyph(upper, lower) {
+	if (upper.a) {
+		if (lower && equalColor(upper, lower)) {
+			return '█';
+		}
+
+		return '▀';
+	}
+
+	if (lower && lower.a) {
+		return '▄';
+	}
+
+	return ' ';
+}
+
 async function render(fileBuffer, {height, width}) {
 	const image = await Jimp.read(fileBuffer);
 	const {bitmap} = image;
@@ -33,45 +77,65 @@ async function render(fileBuffer, {height, width}) {
 
 	const ret = new Array(Math.ceil(bitmap.height / 2));
 	for (let y = 0; y < bitmap.height - 1; y += 2) {
-		const rowIndex = y / 2;
 		let row = '';
-		for (let x = 0; x < bitmap.width; x++) {
-			const {r, g, b, a} = Jimp.intToRGBA(image.getPixelColor(x, y));
-			const {r: r2, g: g2, b: b2, a: a2} = Jimp.intToRGBA(image.getPixelColor(x, y + 1));
+		let prevFgColor = Jimp.intToRGBA(image.getPixelColor(0, y));
+		let prevBgColor = Jimp.intToRGBA(image.getPixelColor(0, y + 1));
+		let buffer = getGlyph(prevFgColor, prevBgColor);
 
-			if (a === 0 && a2 === 0) {
-				// Both pixels are full transparent
-				row += chalk.reset(' ');
-			} else if (a) {
-				// Only upper pixel is full transparent
-				row += chalk.rgb(r2, g2, b2)('▄');
-			} else if (a2) {
-				// Only lower pixel is full transparent
-				row += chalk.rgb(r, g, b)('▀');
-			} else if (r === r2 && g === g2 && b === b2) {
-				// Both pixels has the same color
-				row += chalk.rgb(r, g, b)('█');
-			} else {
-				// Pixels has different colors
-				row += chalk.rgb(r, g, b).bgRgb(r2, g2, b2)('▀');
+		for (let x = 1; x < bitmap.width; x++) {
+			let fgColor = Jimp.intToRGBA(image.getPixelColor(x, y));
+			let bgColor = Jimp.intToRGBA(image.getPixelColor(x, y + 1));
+			const glyph = getGlyph(fgColor, bgColor);
+
+			// Special case where upper pixel is full transparent but lower one not,
+			// set foreground color to the lower pixel one and set background as
+			// transparent since we are drawing a lower half block
+			if (fgColor.a === 0 && bgColor.a > 0) {
+				fgColor = bgColor;
+				bgColor = {a: 0};
 			}
+
+			// TODO optimization: check foreground and background independently and
+			// also the case of upper block full transparent
+			if (equalColor(prevFgColor, fgColor) && equalColor(prevBgColor, bgColor)) {
+				buffer += glyph;
+				continue;
+			}
+
+			row += getBgColor(prevBgColor)(getFgColor(prevFgColor)(buffer));
+
+			prevFgColor = fgColor;
+			prevBgColor = bgColor;
+			buffer = glyph;
 		}
 
-		ret[rowIndex] = row;
+		ret[y / 2] = row + getBgColor(prevBgColor)(getFgColor(prevFgColor)(buffer));
 	}
 
 	// Image has an odd number of rows
 	if (height % 2) {
 		const y = height - 1;
-		const rowIndex = y / 2;
-		let row = '';
-		for (let x = 0; x < bitmap.width; x++) {
-			const {r, g, b} = Jimp.intToRGBA(image.getPixelColor(x, y));
 
-			row += chalk.rgb(r, g, b)('▀');
+		let row = '';
+		let prevFgColor = Jimp.intToRGBA(image.getPixelColor(0, y));
+		let buffer = getGlyph(prevFgColor);
+
+		for (let x = 1; x < bitmap.width; x++) {
+			const fgColor = Jimp.intToRGBA(image.getPixelColor(x, y));
+			const glyph = getGlyph(fgColor);
+
+			if (equalColor(prevFgColor, fgColor)) {
+				buffer += glyph;
+				continue;
+			}
+
+			row += getFgColor(prevFgColor)(buffer);
+
+			prevFgColor = fgColor;
+			buffer = glyph;
 		}
 
-		ret[rowIndex] = row;
+		ret[y / 2] = row + getFgColor(prevFgColor)(buffer);
 	}
 
 	return ret.join('\n');
