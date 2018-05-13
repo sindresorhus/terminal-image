b2a18ae7542f4aa2a9c63d36e62acddf6604935e
Faisal KANOUT <faisalkanout@faisals-macbook-pro-2.home>
2018-05-13 16:24:09 +0200

    ADD - test

--- test.js
+++ test.js
@@ -11,3 +11,8 @@ test('.file()', async t => {
 	const result = await terminalImage.file('fixture.jpg');
 	t.is(typeof result, 'string');
 });
+
+test('.url()', async t => {
+	const result = await terminalImage.url('https://upload.wikimedia.org/wikipedia/commons/a/a4/Google_garage.JPG');
+	t.is(typeof result, 'string');
+});
