80ad12719b1790b23306c1cf5a31adb4828192d2
Daulet Urazalinov <d.urazalinov@gmail.com>
2020-04-20 11:32:15 -0400

    Updated documentation

--- index.d.ts
+++ index.d.ts
@@ -22,14 +22,14 @@ declare const terminalImage: {
 
 	/**
 	Display images in the terminal. Please note that the image will always be scaled to fit the size of the terminal.
+
 	Optionally, you can specify the height and/or width to scale the image.
 	That can be either the percentage of the terminal window or number of rows and/or columns.
 	Please note that the image will always be scaled to fit the size of the terminal.
 	If width and height are not defined, by default the image will take the width and height of the terminal.
 	It is recommended to use the percentage option.
-	That can be either the percentage of the terminal window or number of rows and/or columns.
-	By default, aspect ratio is always maintained. If you don't want to maintain aspect ratio, set `preserveAspectRatio` to false.
-	However, your image will be scaled to fit the size of the terminal.
+	You can set width and/or height as columns and/or rows of the terminal window as well.
+	By default, aspect ratio is always maintained. If you don't want to maintain aspect ratio, set preserveAspectRatio to false.
 
 	@param filePath - File path to the image.
 	@param options - Image rendering options.
@@ -38,12 +38,17 @@ declare const terminalImage: {
 	@param options.preserveAspectRatio - Optional: Whether to maintain image aspect ratio or not. Default: true.
 	@returns The ANSI escape codes to display the image.
 
+
+
 	@example
 	```
-	import terminalImage = require('terminal-image');
+	const terminalImage = require('terminal-image');
 
 	(async () => {
+		console.log(await terminalImage.file('unicorn.jpg'));
 		console.log(await terminalImage.file('unicorn.jpg', {width: '50%', height: '50%'}));
+		console.log(await terminalImage.file('unicorn.jpg', {width: 50 }));
+		console.log(await terminalImage.file('unicorn.jpg', {width: 70, height: 50, preserveAspectRatio: false}));
 	})();
 	```
 	*/
