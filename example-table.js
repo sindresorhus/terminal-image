7207828b0f00ece5a1a264d2d947942a27876e31
Umiko <231703339+umikoio@users.noreply.github.com>
2025-09-21 21:00:12 -0500

    Fix npm tests

--- example-table.js
+++ example-table.js
@@ -1,6 +1,6 @@
-import terminalImage from './index.js';
 import Table from 'cli-table3';
+import terminalImage from './index.js';
 
 const table = new Table({head: ['Image', 'Description']});
-table.push([await terminalImage.file('fixture.jpg', {width: '100%', isTable: true}), 'Cute Dog'])
+table.push([await terminalImage.file('fixture.jpg', {width: '100%', isTable: true}), 'Cute Dog']);
 console.log(table.toString());
